64f537f32a5bb6a8bd9aaacaa51231d1b317cf95
Andrew Owens <AndrewOwens121@users.noreply.github.com>
2021-11-11 17:39:01 +0000

    Added beginnings of basic password functionality

---
+++ .idea/main.py
@@ -0,0 +1,11 @@
+from csv_service import CSVService
+
+password = "Secret"
+
+usrpswrd=input("Please enter password >>")
+
+if usrpswrd == password:
+    print("Correct password")
+
+else:
+    print("Incorrect password")
